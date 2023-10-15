a11063d83116794dfe8aa865f68cce81046a6aac
Viviana <vivibecker.andrade@gmail.com>
2023-10-15 19:05:09 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+function calcularNivel(vitorias, derrotas) {
+    const saldoVitorias = vitorias - derrotas;
+    let nivel;
+
+    if (vitorias < 10) {
+        nivel = "Ferro";
+    } else if (vitorias >= 11 && vitorias <= 20) {
+        nivel = "Bronze";
+    } else if (vitorias >= 21 && vitorias <= 50) {
+        nivel = "Prata";
+    } else if (vitorias >= 51 && vitorias <= 80) {
+        nivel = "Ouro";
+    } else if (vitorias >= 81 && vitorias <= 90) {
+        nivel = "Diamante";
+    } else if (vitorias >= 91 && vitorias <= 100) {
+        nivel = "Lendário";
+    } else {
+        nivel = "Imortal";
+    }
+
+    return { saldoVitorias, nivel };
+}
+
+// Exemplo de uso:
+const vitorias = 35;
+const derrotas = 15;
+
+const { saldoVitorias, nivel } = calcularNivel(vitorias, derrotas);
+
+console.log(`O Herói tem um saldo de ${saldoVitorias} está no nível de ${nivel}`);
